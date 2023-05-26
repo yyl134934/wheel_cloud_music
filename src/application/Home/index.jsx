42558f4f1e00f00d3a868ba0789b373cc08f5efb
yaoc <yyl134934@163.com>
2023-05-26 23:27:44 +0800

    feat(home):更新Home组件

--- src/application/Home/index.jsx
+++ src/application/Home/index.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React from 'react';
+import { Top, Tab, TabItem } from './style';
+import { NavLink, Outlet } from 'react-router-dom';
 
-function Home(params) {
-        return <div>Home</div>
+function Home(props) {
+  return (
+    <div>
+      <Top>
+        <span className='iconfont menu'>&#xe65c;</span>
+        <span className='title'>WebApp</span>
+        <span className='iconfont search'>&#xe62b;</span>
+      </Top>
+      <Tab>
+        <NavLink to={'recommend'} className={({ isActive }) => (isActive ? 'selected' : '')}>
+          <TabItem>
+            <span>推荐</span>
+          </TabItem>
+        </NavLink>
+        <NavLink to={'singers'} className={({ isActive }) => (isActive ? 'selected' : '')}>
+          <TabItem>
+            <span>歌手</span>
+          </TabItem>
+        </NavLink>
+        <NavLink to={'rank'} className={({ isActive }) => (isActive ? 'selected' : '')}>
+          <TabItem>
+            <span>排行榜</span>
+          </TabItem>
+        </NavLink>
+      </Tab>
+      {/* 渲染子路由 */}
+      <Outlet />
+    </div>
+  );
 }
 
 export default React.memo(Home);
