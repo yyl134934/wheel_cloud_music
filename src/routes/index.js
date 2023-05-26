ae07f49515f30651ef6d642247e4b85ddee3fc82
yaoc <yyl134934@163.com>
2023-05-26 23:27:03 +0800

    feat(router):更新router配置

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,33 +1,35 @@
 //routes/index.js
 import React from 'react';
+import { Navigate } from 'react-router-dom';
 import Home from '../application/Home';
 import Recommend from '../application/Recommend';
 import Singers from '../application/Singers';
 import Rank from '../application/Rank';
 import NoFound from '../application/NoFound';
 
-export const pathConfig=[
-        {
-          path: "/",
-          exact: true,
-          element: <Home />,
-        },
-        {
-          path: "/recommend",
-          element: <Recommend/>
-        },
-        {
-          path: "/singers",
-          element: <Singers/>
-        },
-        {
-          path: "/rank",
-          element: <Rank/>
-        },
-        {
-          path: "*",
-          element: <NoFound/>
-        }
-      ];
+export const pathConfig = [
+  {
+    path: '/',
+    element: <Home />,
+    children: [
+      {
+        path: '',
+        element: <Navigate to='/recommend' replace={true} />,
+      },
+      {
+        path: 'recommend',
+        element: <Recommend />,
+      },
+      {
+        path: 'singers',
+        element: <Singers />,
+      },
+      {
+        path: 'rank',
+        element: <Rank />,
+      },
+    ],
+  },
+];
 
 export default pathConfig;
