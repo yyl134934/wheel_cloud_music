2c70ec05519821e5a2dffc6394d6b4100f397e23
yaoc <yyl134934@163.com>
2023-05-28 21:15:55 +0800

    feat(singer):歌手界面新增横向分类滚动条

--- src/application/Singers/index.jsx
+++ src/application/Singers/index.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from 'react';
+import Horizon from '../../baseUI/horizon-item';
+import { categoryTypes, alphaTypes } from '../../api/config';
+import { NavContainer } from './style';
 
 function Singers(params) {
-        return <div>Singers</div>
+  let [category, setCategory] = useState('');
+  let [alpha, setAlpha] = useState('');
+
+  let handleUpdateAlpha = (val) => {
+    setAlpha(val);
+  };
+
+  let handleUpdateCatetory = (val) => {
+    setCategory(val);
+  };
+
+  return (
+    <NavContainer>
+      <Horizon
+        list={categoryTypes}
+        title={'分类 (默认热门):'}
+        handleClick={handleUpdateCatetory}
+        oldVal={category}
+      ></Horizon>
+      <Horizon list={alphaTypes} title={'首字母:'} handleClick={handleUpdateAlpha} oldVal={alpha}></Horizon>
+    </NavContainer>
+  );
 }
 
 export default React.memo(Singers);
