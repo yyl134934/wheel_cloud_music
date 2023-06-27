2b2c51ab118accada259197d34257ced93a182cc
yaoc <yyl134934@163.com>
2023-06-27 17:27:13 +0800

    feat(jsdoc):引入jsdoc，增强代码可读性

--- src/api/requst.js
+++ src/api/requst.js
@@ -1,18 +1,36 @@
 import { axiosInstance, categoryMap } from './config';
 
+/**
+ * 获取轮播图
+ * @returns banner
+ */
 export const getBannerRequest = () => {
   return axiosInstance.get('/banner');
 };
 
+/**
+ * 获取推荐歌单
+ * @returns data
+ */
 export const getRecommendListRequest = () => {
   return axiosInstance.get('/personalized');
 };
 
-export const getHotSingerListRequest = (count) => {
-  return axiosInstance.get(`/top/artists?offset=${count}`);
+/**
+ * 热门歌手
+ * @param {number} offset 偏移数量 , 用于分页 , 默认 为 0
+ * @returns
+ */
+export const getHotSingerListRequest = (offset = 0) => {
+  return axiosInstance.get(`/top/artists?offset=${offset}`);
 };
 
-function getSingerParams(category) {
+/**
+ * 适应接口变更——category拆分为type&area
+ * @param {string} category 分类
+ * @returns {string} paramsStr
+ */
+function getSingerParams(category = '') {
   if (category) {
     const { type, area } = categoryMap.get(category);
 
@@ -21,26 +39,52 @@ function getSingerParams(category) {
   return '';
 }
 
-export const getSingerListRequest = (category, alpha, count) => {
+/**
+ * 歌手分类列表
+ * @param {string} category 分类
+ * @param {string} alpha 按首字母索引查找参数
+ * @param {number} offset 偏移数量 , 用于分页 , 默认 为 0
+ * @returns category和alpha都为空：热门歌手；category和alpha其中一个有值：歌手分类
+ */
+export const getSingerListRequest = (category = '', alpha = '', offset = 0) => {
   if (!category && !alpha) {
-    return getHotSingerListRequest(count);
+    return getHotSingerListRequest(offset);
   }
   const paramsStr = getSingerParams(category);
-  return axiosInstance.get(`/artist/list?${paramsStr}&initial=${alpha.toLowerCase()}&offset=${count}`);
+  return axiosInstance.get(`/artist/list?${paramsStr}&initial=${alpha.toLowerCase()}&offset=${offset}`);
 };
 
+/**
+ * 获取排行榜
+ * @returns
+ */
 export const getRankListRequest = () => {
   return axiosInstance.get(`/toplist/detail`);
 };
 
-export const getAlbumDetailRequest = (id) => {
+/**
+ * 获取歌单详情
+ * @param {string} id 歌单、专辑、排行榜id
+ * @returns
+ */
+export const getAlbumDetailRequest = (id = '') => {
   return axiosInstance.get(`/playlist/detail?id=${id}`);
 };
 
-export const getSingerInfoRequest = (id) => {
+/**
+ * 获取歌手信息
+ * @param {string} id 歌手id
+ * @returns
+ */
+export const getSingerInfoRequest = (id = '') => {
   return axiosInstance.get(`/artists?id=${id}`);
 };
 
-export const getSongUrl = (id) => {
+/**
+ * 获取歌曲URL
+ * @param {string} id 歌曲id
+ * @returns
+ */
+export const getSongUrl = (id = '') => {
   return `http://music.163.com/song/media/outer/url?id=${id}.mp3`;
 };
--- src/api/utils.js
+++ src/api/utils.js
@@ -1,7 +1,7 @@
 /**
  * 处理数量单位——万、亿
- * @param {*} count
- * @returns
+ * @param {number} count
+ * @returns {string} count
  */
 export const getCount = (count) => {
   if (count < 0) return;
@@ -16,10 +16,10 @@ export const getCount = (count) => {
 
 /**
  * 处理数据，找出第一个没有歌名的排行榜的索引
- * @param {*} rankList
- * @returns
+ * @param {Object[]} rankList 排行榜列表
+ * @returns {number} index
  */
-export const filterIndex = (rankList) => {
+export const filterIndex = (rankList = []) => {
   for (let i = 0; i < rankList.length - 1; i++) {
     if (rankList[i].tracks.length && !rankList[i + 1].tracks.length) {
       return i + 1;
@@ -27,14 +27,20 @@ export const filterIndex = (rankList) => {
   }
 };
 
-// 处理歌手列表拼接歌手名字
+/**
+ * 处理歌手列表拼接歌手名字
+ * @param {any[]} list
+ * @returns {string} singerName
+ */
 export const getName = (list = []) => {
   return list.reduce((prev, { name }, index) => {
     return (prev += index === 0 ? name : '/' + name);
   }, '');
 };
 
-// 给 css3 相关属性增加浏览器前缀，处理浏览器兼容性问题
+/**
+ * 给 css3 相关属性增加浏览器前缀，处理浏览器兼容性问题
+ */
 let elementStyle = document.createElement('div').style;
 
 let vendor = (() => {
@@ -54,6 +60,11 @@ let vendor = (() => {
   return false;
 })();
 
+/**
+ * 前缀
+ * @param {string} style
+ * @returns {string|boolean} style
+ */
 export function prefixStyle(style) {
   if (vendor === false) {
     return false;
@@ -64,7 +75,11 @@ export function prefixStyle(style) {
   return vendor + style.charAt(0).toUpperCase() + style.slice(1);
 }
 
-//转换歌曲播放时间
+/**
+ * 转换歌曲播放时间
+ * @param {number} interval 秒
+ * @returns {string} minute:second
+ */
 export const formatPlayTime = (interval) => {
   interval = interval | 0; // |0表示向下取整
   const minute = (interval / 60) | 0;
@@ -72,35 +87,59 @@ export const formatPlayTime = (interval) => {
   return `${minute}:${second}`;
 };
 
+/**
+ * 获取指定范围内的随机数
+ * @param {number} min 最小值
+ * @param {number} max 最大值
+ * @returns {number} random
+ */
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-// 随机算法
+/**
+ * 随机算法
+ * @param {any[]} arr 等待被洗牌的数组
+ * @returns {any[]} shuffledArr
+ */
 export function shuffle(arr) {
-  const new_arr = [];
+  const newArr = [];
 
   arr.forEach((item) => {
-    new_arr.push(item);
+    newArr.push(item);
   });
 
-  for (let i = 0; i < new_arr.length; i++) {
+  for (let i = 0; i < newArr.length; i++) {
     const j = getRandomInt(0, i);
-    const t = new_arr[i];
-    new_arr[i] = new_arr[j];
-    new_arr[j] = t;
+    const t = newArr[i];
+    newArr[i] = newArr[j];
+    newArr[j] = t;
   }
 
-  return new_arr;
+  return newArr;
 }
 
-// 找到当前的歌曲索引
+/**
+ * @typedef Song 歌曲
+ */
+
+/**
+ * 找到当前的歌曲索引
+ * @param {Song} song
+ * @param {Song[]} list
+ * @returns {number} index
+ */
 export const findIndex = (song, list) => {
   return list.findIndex((item) => {
     return song.id === item.id;
   });
 };
 
-export const isEmptyObject = (params) => {
-  return typeof params === 'object' && !Object.keys(params).length;
+/**
+ * 判断对象是否为空
+ * @param {Object} obj 对象
+ * @returns {boolean} isEmpty
+ */
+export const isEmptyObject = (obj) => {
+  return typeof params === 'object' && !Object.keys(obj).length;
 };
